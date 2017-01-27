a34803c4023cdb72134ba20cc166f1d0494d186f
bskinny <brian.williams@mayalane.com>
2017-01-26 19:26:26 -0500

    HD-39 Sign the instagram api requests. Do not enforce on Helodali's Instagram application configuration until tests are successful

--- src/clj/helodali/instagram.clj
+++ src/clj/helodali/instagram.clj
@@ -8,19 +8,40 @@
             [clj-time.core :refer [now days ago]]
             [clj-time.format :refer [parse unparse formatters]]
             [helodali.common :refer [coerce-int fix-date keywordize-vals]]
-            [helodali.db :refer [get-account update-user-table]]
-            [clojure.pprint :refer [pprint]]))
+            [helodali.db :refer [get-account update-user-table]])
+  (:import (javax.crypto Mac)
+           (javax.crypto.spec SecretKeySpec)))
 
 (def client
   {:id (get (System/getenv) "HD_INSTAGRAM_CLIENT_ID")
    :secret (get (System/getenv) "HD_INSTAGRAM_CLIENT_SECRET")
    :redirect-uri (get (System/getenv) "HD_INSTAGRAM_REDIRECT_URI")})
 
-(def options {:timeout 900  ;; ms
+(def options {:timeout 2000  ;; ms
               ; :debug true :debug-body true
-              :as :json})  ;; Try to automatically coerce the output based on the content-type
+              :as :auto})  ;; Try to automatically coerce the output based on the content-type
 
 (def base-url "https://api.instagram.com")
+
+(defn- create-params-string
+  "Given a map of params {:count 10 :access_token \"123\"},
+   convert it to the format ready for signing: access_token=123&count=10"
+  [params]
+  (let [sorted-params (sort-by #(name (key %)) params) ;; Creates a list of vector tuples
+        params-strings (map (fn [v] (str (name (first v)) "=" (second v))) sorted-params)]
+    (clojure.string/join "&" params-strings)))
+
+(defn- sign-request
+  "Sign the request according to https://www.instagram.com/developer/secure-api-requests/
+   That is: sign <endpoint>|param1=value1|param2=value2 where params are placed in sorted order.
+   The incoming params-string looks like \"access_token=123&count=10\""
+  [endpoint params-string]
+  (let [key-spec (SecretKeySpec. (.getBytes (:secret client)) "HmacSHA256")
+        mac (doto (Mac/getInstance "HmacSHA256")
+               (.init key-spec))
+        sign-this (str endpoint "|" (clojure.string/replace params-string "&" "|"))
+        bytes (.doFinal mac (.getBytes sign-this))]
+    (apply str (map #(format "%x" %) bytes))))
 
 (defn- convert-to-item
   "Build a helodali item representing the instagram media item"
@@ -38,10 +59,17 @@
   "Return recent media for 'self'
    https://www.instagram.com/developer/endpoints/users/#get_users_media_recent_self"
   [access-token]
-  (let [resp (-> (http/get (str base-url "/v1/users/self/media/recent/?access_token=" access-token) {:as :auto})
-                 (:body))]
+  (let [endpoint "/users/self/media/recent"
+        params-string (create-params-string {:access_token access-token})
+        sig (sign-request endpoint params-string)
+        resp (try+
+               (-> (http/get (str base-url "/v1" endpoint "?" params-string "&sig=" sig) options)
+                   (:body))
+               (catch Object _
+                 (pprint (str (:throwable &throw-context) "unexpected error"))
+                 (throw+)))]
     (if (not= 200 (:code (:meta resp)))
-      (pprint (str "Error fetching media: " resp))
+      (pprint (str "Error fetching media: " resp)) ;; Likely will not get here due to try/catch
       (let [media (:data resp)]
         (map convert-to-item media)))))
 
@@ -74,7 +102,6 @@
     ;; {:access_token \"1234", :user {:username \"helodali\", :bio \"Painter...\", :website \"http://www.mayalane.com/\",
     ;;                                :profile_picture \"https://scontent-lga3-1.cdninstagram.com/....jpg\",
     ;;                                :full_name \"Brian Williams\", :id \"234"}}
-    (pprint (str "request-access-token resp: " resp))
     (if (:access_token resp)
       (update-user-table :accounts uref nil {:instagram-access-token (:access_token resp)
                                              :instagram-user (:user resp)}))))
