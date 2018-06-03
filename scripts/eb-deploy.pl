c6dbc15a7c8dab3927a78bc275bec79ae1cf10d0
bskinny <brian.williams@mayalane.com>
2018-06-02 21:30:55 -0400

    Add deployment script

--- scripts/eb-deploy.pl
+++ scripts/eb-deploy.pl
@@ -3,7 +3,6 @@
 # We are going to alter project.clj and resounse/public/index.html and then checkout them out again.
 # So make sure it is not currently modified.
 @diffs = qx(git diff --name-only);
-print "Diffs: @diffs\n";
 foreach $file (@diffs) {
 	chomp($file);
 	if ($file =~ /^project.clj$/ or $file =~ /^resources\/public\/index.html$/) {
@@ -11,3 +10,21 @@ foreach $file (@diffs) {
 		exit(1);
 	}
 }
+
+$ct = time(); # Seconds since epoch
+qx(perl -p -i -e "s/app.js/app-$ct.js/g" project.clj);
+qx(perl -p -i -e "s/app.js/app-$ct.js/g" resources/public/index.html);
+
+# Build the war file and rename it
+qx(lein clean);
+qx(lein with-profile webapp ring uberwar);
+$war = "helodali-$ct.war";
+qx(mv target/helodali.war "target/$war");
+
+# Deploy the war
+print qx(aws s3 cp "target/$war" s3://elasticbeanstalk-us-east-1-128225160927/);
+print qx(aws elasticbeanstalk create-application-version --application-name helodali --version-label "v$ct" --source-bundle S3Bucket="elasticbeanstalk-us-east-1-128225160927",S3Key="$war");
+print qx(aws elasticbeanstalk update-environment --application-name helodali --environment-name Helodali-env --version-label "v$ct");
+
+qx(git checkout project.clj resources/public/index.html);
+
