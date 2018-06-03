5597e4d15b4b4188c974cd5b728ae427b444601e
bskinny <brian.williams@mayalane.com>
2018-06-02 20:50:28 -0400

    Test commit

--- project.clj
+++ project.clj
@@ -89,7 +89,7 @@
              {:source-paths ["src/cljs" "src/cljc" "env/prod/cljs"]
               :jar true
               :compiler {:main            helodali.prod
-                         :output-to       "resources/public/js/compiled/app.js" ;; This filename is changed by eb-deploy.sh
+                         :output-to       "resources/public/js/compiled/app.js" ;; This filename is changed by eb-deploy.pl for cache busting
                          :optimizations   :advanced
                          :closure-defines {goog.DEBUG false}
                          :pretty-print    false}}}}}
