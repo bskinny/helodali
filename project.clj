a99535742a443a7c307bdfbf91b15c9a51847959
bskinny <brian@mayalane.com>
2018-10-29 20:47:11 -0400

    Fix newline

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
                  [reagent "0.8.0"]
                  [re-frame "0.10.5"]
                  [cljs-ajax "0.7.3"]
-                 [day8.re-frame/http
-                  -fx "0.1.6"]
+                 [day8.re-frame/http-fx "0.1.6"]
                  [joda-time "2.9.9"]
                  [amazonica "0.3.125" :exclusions [com.amazonaws/aws-java-sdk
                                                    com.amazonaws/amazon-kinesis-client]]
