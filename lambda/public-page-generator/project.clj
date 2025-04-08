27eff19a619f9a8c6dcb93f48f5ce7125f093b1b
Brian Williams <brian@mayalane.com>
2025-04-08 18:43:01 -0400

    Bump deps for public-page-generator; using JDK 21 now as Lambda runtime

--- lambda/public-page-generator/project.clj
+++ lambda/public-page-generator/project.clj
@@ -1,19 +1,19 @@
 (defproject public-pages-generator "1.0.1"
-  :description "FIXME: write description"
+  :description "AWS Lambda function to generate public pages"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [com.amazonaws/aws-lambda-java-core "1.2.0"]
-                 [amazonica "0.3.130" :exclusions [com.amazonaws/aws-java-sdk
+                 [amazonica "0.3.134" :exclusions [com.amazonaws/aws-java-sdk
                                                    com.amazonaws/amazon-kinesis-client]]
-                 [com.amazonaws/aws-java-sdk-core "1.11.347"]
-                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.347"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.347"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.561"]
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.561"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.561"]
                  [org.clojure/data.json "0.2.1"]
-                 [clojure.java-time "0.3.2"]
-                 [com.amazonaws/aws-java-sdk-sns "1.11.347"]
-                 [com.amazonaws/aws-java-sdk-cloudfront "1.11.347"]]
+                 [clojure.java-time "1.4.3"]
+                 [com.amazonaws/aws-java-sdk-sns "1.11.561"]
+                 [com.amazonaws/aws-java-sdk-cloudfront "1.11.561"]]
   :min-lein-version "2.6.1"
 
   :source-paths ["src/clj"]
